ea0902e841d707dec3fbe03d6cad521f5e7f0df7
Leandro Caballero <caballero_leandro@hotmail.com>
2018-05-09 19:12:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
 console.log(message)
+
+//test
+
+var name1 = "Caballero"
+var money = "1 million"
+var message2 = '${name1} is ${money} richier now'
+
+module.exports = {name1, money, message2}
+
+console.log(message2)
+
